7352e637bf44bf80d4302bc548156d5fba239792
david parsons <orc@pell.portland.or.us>
2018-02-04 22:01:34 -0800

    allow arg[1] as the start time, then do all open/closes as clock times rather than plus times

--- open-close.c
+++ open-close.c
@@ -4,25 +4,31 @@
 #include <math.h>
 
 static void
-totime(int val)
+totime(int val, char sep)
 {
     int dy = val / (60*24);
     int hr = (val / 60) % 24;
     int mn = val % 60;
 
     if ( dy > 0 )
-	printf("%d:", dy);
+	printf("%d ", dy);
     if ( dy > 0 || hr > 0 )
-	printf("%dh", hr);
+	printf("%d%c", hr, sep);
     printf("%02d", mn);
 }
 
 
 static void
-openclose(float open, float close)
+openclose(int start, float open, float close)
 {
-    printf("open +"); totime((int)open);
-    printf(" / close +"); totime((int)close);
+    if ( start > 0 ) {
+	printf("open "); totime(start + open, ':');
+	printf(" / close "); totime(start + close, ':');
+    }
+    else {
+	printf("open +"); totime((int)open, 'h');
+	printf(" / close +"); totime((int)close, 'h');
+    }
     putchar('\n');
 }
 
@@ -35,6 +41,12 @@ char **argv;
     int miles;
     int km;
     float val;
+    int hr = 0, min = 0;
+
+    if ( (argc > 1) && sscanf(argv[1], "%d:%02d", &hr, &min) == 2 ) {
+	++argv, --argc;
+	min += (hr*60);
+    }
 
     for ( x=1; x<argc; x++ ) {
 	int e = strlen(argv[x])-1;
@@ -67,9 +79,9 @@ char **argv;
 
 	printf("%.8s: ", argv[x]);
 	if ( val < 1 )
-	    openclose(0.0, 60.0);
+	    openclose(min, 0.0, 60.0);
 	else
-	    openclose( val * 2, val * 4 );
+	    openclose(min, val * 2, val * 4 );
     }
     exit(0);
 }
